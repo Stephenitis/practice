6c02fcb1089cf93694cc023e6c15eac35c495a63
Your Name <nguyenhstephen@gmail.com>
2012-11-27 17:06:42 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripp::Application.config.secret_token = 'd139550a2d1d35047335676f1aeec62c68f5eeffb7cc425e6f2938cb24c305cfadde6cf4d11488bcf88fbd25e60ac99f68aad172d335a29939e9bd60dc537568'
